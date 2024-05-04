550cac56d54b738fa5daa36614ea454fca30cf2f
Andreas Wiese <andreas.wiese@tum.de>
2024-05-04 19:18:57 +0200

    test

--- test_someexamples.py
+++ test_someexamples.py
@@ -4,7 +4,7 @@ import pytest
 
 def test_add():
     assert add(2, 3) == 5
-    assert add('space', 'ship') == 'spaceship'
+    assert add('space', 'ship') == 'spaceship' 
 
 # uncomment the following test in step 5
 #def test_subtract():
